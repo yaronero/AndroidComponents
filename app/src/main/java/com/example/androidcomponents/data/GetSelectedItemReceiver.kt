28b7c055f29d85cae679908d6e47351147e7436e
yaronero <yaroslavzakh.prog@gmail.com>
2022-07-19 21:17:47 +0300

    Final version

--- app/src/main/java/com/example/androidcomponents/data/GetSelectedItemReceiver.kt
+++ app/src/main/java/com/example/androidcomponents/data/GetSelectedItemReceiver.kt
@@ -3,22 +3,21 @@ package com.example.androidcomponents.data
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
-import com.example.androidcomponents.utils.LAST_SELECTED_ITEM_ID
-import com.example.androidcomponents.utils.PREFS_ITEM
+import com.example.androidcomponents.presentation.MainActivity
 
 class GetSelectedItemReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        val sharedPrefs = context?.getSharedPreferences(PREFS_ITEM, Context.MODE_PRIVATE)
 
-        val selectedItemId = sharedPrefs?.getInt(LAST_SELECTED_ITEM_ID, UNDEFINED_ID)
+        val mainActivityIntent = Intent(context, MainActivity::class.java).apply {
+            putExtra(IS_FROM_RECEIVER, true)
+        }
 
-        Log.d("BR_TRIGGER", "GetSelectedItemReceiver: last selected item id is $selectedItemId")
+        context?.startActivity(mainActivityIntent)
     }
 
     companion object {
-        private const val UNDEFINED_ID = -1
+        const val IS_FROM_RECEIVER = "IS_FROM_RECEIVER"
 
         const val ACTION_SHOW_LAST_SELECTED =
             "com.example.action.androidcomponents.SHOW_LAST_SELECTED"
--- app/src/main/java/com/example/androidcomponents/presentation/MainActivity.kt
+++ app/src/main/java/com/example/androidcomponents/presentation/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.androidcomponents.presentation
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
@@ -8,8 +9,13 @@ import com.example.androidcomponents.data.ForegroundService
 import com.example.androidcomponents.databinding.ActivityMainBinding
 
 import android.content.IntentFilter
+import androidx.fragment.app.Fragment
 import com.example.androidcomponents.data.GetSelectedItemReceiver
 import com.example.androidcomponents.presentation.itemlist.ItemListFragment
+import com.example.androidcomponents.presentation.selecteditem.SelectedItemFragment
+import com.example.androidcomponents.utils.LAST_SELECTED_ITEM_ID
+import com.example.androidcomponents.utils.PREFS_ITEM
+import java.lang.reflect.UndeclaredThrowableException
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,10 +34,43 @@ class MainActivity : AppCompatActivity() {
         registerReceiver(myBroadcastReceiver, intentFilter)
 
         if (savedInstanceState == null) {
+            launchFragment(ItemListFragment.newInstance())
+        }
+
+        val isIntentFromReceiver =
+            intent.getBooleanExtra(GetSelectedItemReceiver.IS_FROM_RECEIVER, false)
+
+        if (isIntentFromReceiver) {
+            startFragmentByReceiver()
+        }
+    }
+
+    private fun startFragmentByReceiver() {
+        val sharedPrefs = getSharedPreferences(PREFS_ITEM, Context.MODE_PRIVATE)
+        val selectedItemId =
+            sharedPrefs?.getInt(LAST_SELECTED_ITEM_ID, UNDEFINED_ID) ?: UNDEFINED_ID
+
+        if (selectedItemId != UNDEFINED_ID) {
+            launchFragment(SelectedItemFragment.newInstance(selectedItemId), true)
+        }
+    }
+
+    private fun launchFragment(fragment: Fragment, addToBackStack: Boolean = false) {
+        if (addToBackStack){
             supportFragmentManager
                 .beginTransaction()
-                .replace(R.id.container, ItemListFragment.newInstance())
+                .replace(R.id.container, fragment)
+                .addToBackStack(null)
+                .commit()
+        } else {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, fragment)
                 .commit()
         }
+    }
+
+    companion object {
+        private const val UNDEFINED_ID = -1
     }
 }
